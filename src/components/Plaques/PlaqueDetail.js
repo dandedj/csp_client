6cdf4c17ec5f49a12d27377f7e155a39239df127
David Dandeneau <davedandeneau@gmail.com>
2023-11-30 11:38:39 -0800

    Cleanup

--- src/components/Plaques/PlaqueDetail.js
+++ src/components/Plaques/PlaqueDetail.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { Card, Row, Col, CardHeader, Spinner } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { Card, Row, Col, CardHeader, Spinner } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { PlaquesService } from '../../services/PlaquesService';
+import { PlaquesService } from "../../services/PlaquesService";
 
 const PlaqueDetail = () => {
     const [plaque, setPlaque] = useState(null);
@@ -14,15 +14,19 @@ const PlaqueDetail = () => {
         setLoading(true);
         const plaquesService = new PlaquesService();
 
-        plaquesService.getPlaqueById(id)
+        plaquesService
+            .getPlaqueById(id)
             .then((res) => {
                 setPlaque(res[0]);
                 console.log("Plaque: ", JSON.stringify(res));
                 setLoading(false);
             })
             .catch((error) => {
-                console.error('There has been a problem with your fetch operation:', error);
-                setError('There has been a problem with your fetch operation');
+                console.error(
+                    "There has been a problem with your fetch operation:",
+                    error
+                );
+                setError("There has been a problem with your fetch operation");
                 setLoading(false);
             });
     }, [id]);
@@ -51,31 +55,50 @@ const PlaqueDetail = () => {
                     <Card.Body>
                         <Card.Title>Plaque Information</Card.Title>
                         <Card.Text>
-                            <strong>Id: </strong><span className="badge bg-secondary">{plaque.id}</span><br />
-                            <strong>Location: </strong> <span className="badge bg-success">{plaque.latitude}</span>, <span className="badge bg-success">{plaque.longitude}</span> with bearing <span className="badge bg-secondary">{plaque.bearing}&deg;</span> <br />
-                            <img className="mb-3 rounder img-fluid" src={plaque.image_url} alt="" /><br />
+                            <strong>Id: </strong>
+                            <span className="badge bg-secondary">
+                                {plaque.id}
+                            </span>
+                            <br />
+                            <strong>Location: </strong>{" "}
+                            <span className="badge bg-success">
+                                {plaque.latitude}
+                            </span>
+                            ,{" "}
+                            <span className="badge bg-success">
+                                {plaque.longitude}
+                            </span>{" "}
+                            with bearing{" "}
+                            <span className="badge bg-secondary">
+                                {plaque.bearing}&deg;
+                            </span>{" "}
+                            <br />
+                            <img
+                                className="mb-3 rounder img-fluid"
+                                src={plaque.image_url}
+                                alt=""
+                            />
+                            <br />
                             <Row>
-                            {plaque.text.map((text, index) => (
-                                <Col key={index}>
-                                    <Card>
-                                        <CardHeader>Plaque</CardHeader>
-                                        <Card.Body>
-                                            <Card.Text>
-                                                {text}
-                                            </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            ))}
-                        </Row>
-                        <br /><br />
+                                {plaque.text.map((text, index) => (
+                                    <Col key={index}>
+                                        <Card>
+                                            <CardHeader>Plaque</CardHeader>
+                                            <Card.Body>
+                                                <Card.Text>{text}</Card.Text>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                ))}
+                            </Row>
+                            <br />
+                            <br />
                         </Card.Text>
                     </Card.Body>
                 </Card>
             )}
         </div>
-
-    )
-}
+    );
+};
 
 export default PlaqueDetail;
